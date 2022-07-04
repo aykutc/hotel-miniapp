392fdca63f1f6df2554d97529b9ad3a7b2d9b036
Aykut ÇALIŞKAN <aykut.caliskan@westerops.com>
2022-07-04 12:35:28 +0300

    font update

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,15 @@ class MyDocument extends Document {
             rel="preload"
             as="font"
           /> */}
+          {/*  <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600&display=swap"
+            as="font"
+            type="text/css"
+            crossOrigin
+          ></link> */}
         </Head>
         <body>
           <Main />

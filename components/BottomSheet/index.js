db0fe30a0a4b37d2896b7f7803eb0a12336cddd8
Mahmut Can Kahya <mahmutcan.kahya@westerops.com>
2022-07-06 11:50:23 +0300

    Update index.js

--- components/BottomSheet/index.js
+++ components/BottomSheet/index.js
@@ -19,37 +19,42 @@ const BottomSheet = ({ onDismiss, isOpen, children, title, leftComponent }) => {
   };
 
   useEffect(() => {
-    window.onload = function () {
-      myPane.current = new CupertinoPane(
-        '.cupertino-pane', // Pane container selector
-        {
-          buttonDestroy: false,
-          showDraggable: false,
-          backdrop: true,
-          fitHeight: true,
-          parentElement: 'body', // Parent container
-          breaks: {
-            middle: { enabled: true, bounce: true },
-          },
-          events: {
-            onDidDismiss: onDidDismiss,
-            onWillDismiss: onDismiss,
-          },
-          fastSwipeClose: true,
-          bottomClose: true,
-          lowerThanBottom: false,
-        }
-      );
-
-      if (isOpen) {
-        myPane.current.present({ animate: true });
+    myPane.current = new CupertinoPane(
+      ".cupertino-pane", // Pane container selector
+      {
+        buttonDestroy: false,
+        showDraggable: false,
+        backdrop: true,
+        fitHeight: true,
+        parentElement: "body", // Parent container
+        breaks: {
+          middle: { enabled: true, bounce: true },
+        },
+        events: {
+          onDidDismiss: onDidDismiss,
+          onWillDismiss: onDismiss,
+        },
+        fastSwipeClose: true,
+        bottomClose: true,
+        lowerThanBottom: false,
       }
-    };
+    );
+
+    if (isOpen) {
+      myPane.current.present({ animate: true });
+    }
   }, []);
 
   return (
     <div className="cupertino-pane">
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '24px' }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "24px",
+        }}
+      >
         {leftComponent || <div />}
         <HeaderTitle>{title}</HeaderTitle>
         <Close onClick={onDismiss} />

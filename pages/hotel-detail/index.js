00d53e308f4c612ee60f1cf27c76c54602f820d7
Yiğitcan <yigitarc45@gmail.com>
2022-07-06 10:27:48 +0300

    init

--- pages/hotel-detail/index.js
+++ pages/hotel-detail/index.js
@@ -1,25 +1,27 @@
-import Callendar from "@/components/Calendar";
-import HeaderTitle from "@/components/HeaderTitle";
-import React from "react";
-import FloatingBottomButton from "@/components/FloatingBottomButton";
-import Back from "@/components/icons/Back";
-import OptimizedImage from "@/components/OptimizedImage";
-import styles from "./hotel-detail.module.css";
-import Arrow from "@/components/icons/Arrow";
-import Like from "@/components/icons/Like";
-import Share from "@/components/icons/Share";
-import Star from "@/components/icons/Star";
-import RecommendedCard from "@/components/RecommendedCard";
-import { RecommendedArray } from "data/data";
-import Dining from "@/components/icons/Dining";
-import Fitness from "@/components/icons/Fitness";
-import Spa from "@/components/icons/Spa";
-import Parking from "@/components/icons/Parking";
-import Accessibility from "@/components/icons/Accessibility";
-import Calendar from "@/components/icons/Calendar";
-import Bed from "@/components/icons/Bed";
-import SummaryCard from "@/components/HotelDetails/SummaryCard";
-import Router from "next/router";
+import Callendar from '@/components/Calendar';
+import HeaderTitle from '@/components/HeaderTitle';
+import React from 'react';
+import FloatingBottomButton from '@/components/FloatingBottomButton';
+import Back from '@/components/icons/Back';
+import OptimizedImage from '@/components/OptimizedImage';
+import styles from './hotel-detail.module.css';
+import Arrow from '@/components/icons/Arrow';
+import Like from '@/components/icons/Like';
+import Share from '@/components/icons/Share';
+import Star from '@/components/icons/Star';
+import RecommendedCard from '@/components/RecommendedCard';
+import { RecommendedArray } from 'data/data';
+import Dining from '@/components/icons/Dining';
+import Fitness from '@/components/icons/Fitness';
+import Spa from '@/components/icons/Spa';
+import Parking from '@/components/icons/Parking';
+import Accessibility from '@/components/icons/Accessibility';
+import Calendar from '@/components/icons/Calendar';
+import Bed from '@/components/icons/Bed';
+import SummaryCard from '@/components/HotelDetails/SummaryCard';
+import Router from 'next/router';
+import BottomSheet from '@/components/BottomSheet';
+import RoomsGuests from '@/components/RoomsGuests';
 
 export async function getStaticProps() {
   return {
@@ -28,27 +30,27 @@ export async function getStaticProps() {
 }
 function HotelDetail(props) {
   const [hotelDetail, setHotelDetail] = React.useState({
-    subTitle: "TROJENA",
-    title: "The Ultra-Luxury Mansions",
-    block: "Block A-21",
-    discountPrice: "$114.99",
-    price: "$124.43",
-    img: "hotel1.jpg",
-    rate: "4.9",
-    reviews: "227",
-    location: "Trojena, Neom, Saudi Arabia",
-    phone: "+966 123 456 789",
+    subTitle: 'TROJENA',
+    title: 'The Ultra-Luxury Mansions',
+    block: 'Block A-21',
+    discountPrice: '$114.99',
+    price: '$124.43',
+    img: 'hotel1.jpg',
+    rate: '4.9',
+    reviews: '227',
+    location: 'Trojena, Neom, Saudi Arabia',
+    phone: '+966 123 456 789',
     amenities: [
-      { icon: <Dining />, name: "Dining" },
-      { icon: <Fitness />, name: "Fitness" },
-      { icon: <Spa />, name: "Spa" },
-      { icon: <Parking />, name: "Parking & Transit" },
-      { icon: <Accessibility />, name: "Accessibility" },
+      { icon: <Dining />, name: 'Dining' },
+      { icon: <Fitness />, name: 'Fitness' },
+      { icon: <Spa />, name: 'Spa' },
+      { icon: <Parking />, name: 'Parking & Transit' },
+      { icon: <Accessibility />, name: 'Accessibility' },
     ],
     booking: {
       calander: {
-        date: "Jun 24 - Jun 27",
-        time: "3",
+        date: 'Jun 24 - Jun 27',
+        time: '3',
       },
       room: {
         count: 1,
@@ -58,9 +60,22 @@ function HotelDetail(props) {
     },
   });
 
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsModalOpen(true);
+    }, 1000);
+  }, []);
+
+  console.log('render');
+
   return (
     <div className={styles.container}>
       <div className={styles.headerContainer}>
+        <BottomSheet title="ROOMS & GUESTS" isOpen={isModalOpen} onDismiss={() => setIsModalOpen(false)} leftComponent={'Reset'}>
+          <RoomsGuests />
+        </BottomSheet>
         <div
           className={styles.circle}
           onClick={() => {
@@ -80,8 +95,8 @@ function HotelDetail(props) {
       </div>
       <OptimizedImage
         style={{
-          height: "294px",
-          width: "100%",
+          height: '294px',
+          width: '100%',
           zIndex: -100,
         }}
         src={hotelDetail.img}
@@ -94,24 +109,24 @@ function HotelDetail(props) {
             style={{
               /* marginTop: "-82px", */
 
-              marginBottom: "24px",
+              marginBottom: '24px',
               zIndex: 100,
             }}
             hotelName="The Ultra-Luxury Mansions"
             digitalKeyOnCkick={() => {}}
             messageOnClick={() => {}}
             checkInOnClick={() => {}}
-            confirmCode={"#0099123456"}
+            confirmCode={'#0099123456'}
             totalDay="3"
             checkIn={{
               date: 24,
-              day: "FRI",
-              month: "JUN",
+              day: 'FRI',
+              month: 'JUN',
             }}
             checkOut={{
               date: 27,
-              day: "MON",
-              month: "JUN",
+              day: 'MON',
+              month: 'JUN',
             }}
           />
         ) : (
@@ -138,24 +153,16 @@ function HotelDetail(props) {
               <div className={styles.dateWrapper}>
                 <Calendar />
                 <div className={styles.roomTextWrapper}>
-                  <p className={styles.roomTitle}>
-                    {hotelDetail.booking.calander.date}
-                  </p>
-                  <p className={styles.roomSubTitle}>
-                    {hotelDetail.booking.calander.time} Nights
-                  </p>
+                  <p className={styles.roomTitle}>{hotelDetail.booking.calander.date}</p>
+                  <p className={styles.roomSubTitle}>{hotelDetail.booking.calander.time} Nights</p>
                 </div>
               </div>
               <div className={styles.roomContainerDivider} />
               <div className={styles.roomWrapper}>
                 <Bed />
                 <div className={styles.roomTextWrapper}>
-                  <p className={styles.roomTitle}>
-                    {hotelDetail.booking.room.count} Room
-                  </p>
-                  <p className={styles.roomSubTitle}>
-                    {hotelDetail.booking.room.adult} Adult
-                  </p>
+                  <p className={styles.roomTitle}>{hotelDetail.booking.room.count} Room</p>
+                  <p className={styles.roomSubTitle}>{hotelDetail.booking.room.adult} Adult</p>
                 </div>
               </div>
             </div>
@@ -165,7 +172,7 @@ function HotelDetail(props) {
 
         {/* Map Detail Start  */}
         <div className={styles.mapDetailContainer}>
-          <OptimizedImage src={"map.jpg"} type="jpg" />
+          <OptimizedImage src={'map.jpg'} type="jpg" />
           <div className={styles.mapDetail}>
             <p className={styles.locationText}>{hotelDetail.location}</p>
             <p className={styles.phoneText}>{hotelDetail.phone}</p>
@@ -176,11 +183,7 @@ function HotelDetail(props) {
         {/* About Start  */}
         <div className={styles.aboutContainer}>
           <div className={styles.title}>About</div>
-          <p className={styles.aboutText}>
-            TROJENA will redefine mountain tourism for the world by creating a
-            place based on the principles of ecotourism, highlighting our
-            efforts to preserve nature and enhance the ....
-          </p>
+          <p className={styles.aboutText}>TROJENA will redefine mountain tourism for the world by creating a place based on the principles of ecotourism, highlighting our efforts to preserve nature and enhance the ....</p>
           <span className={styles.readMore}>Read More</span>
           <div className={styles.horizontalDivider}></div>
         </div>
@@ -207,19 +210,7 @@ function HotelDetail(props) {
         <div className={styles.title}>You May Also Like</div>
         <div className={styles.recommendedContainer}>
           {RecommendedArray.map((item) => {
-            return (
-              <RecommendedCard
-                key={item.title}
-                title={item.title}
-                subTitle={item.subTitle}
-                block={item.block}
-                img={item.img}
-                imgWebp={item.imgWebp}
-                price={item.price}
-                discountPrice={item.discountPrice}
-                imageStyles={{ width: "100%", height: 118, display: "block" }}
-              ></RecommendedCard>
-            );
+            return <RecommendedCard key={item.title} title={item.title} subTitle={item.subTitle} block={item.block} img={item.img} imgWebp={item.imgWebp} price={item.price} discountPrice={item.discountPrice} imageStyles={{ width: '100%', height: 118, display: 'block' }}></RecommendedCard>;
           })}
         </div>
         {/* Recommend End  */}

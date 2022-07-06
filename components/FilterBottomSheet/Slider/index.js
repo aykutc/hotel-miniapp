11343466f4bed6d1074a43394da4db85c11e5de9
Beyza <beyza.efe@westerops.com>
2022-07-06 17:30:04 +0300

    Review pages added

--- components/FilterBottomSheet/Slider/index.js
+++ components/FilterBottomSheet/Slider/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styles from "./slider.module.css";
 
 const Slider = () => {
   const [price, setPrice] = useState(40);
@@ -7,10 +6,51 @@ const Slider = () => {
     setPrice(e.target.value);
   };
   return (
-    <div>
-      <input type="range" className={styles.slider} onInput={handleInput} />
-      <p>Value: {price}</p>
-    </div>
+    <>
+      <div>
+        <input type="range" className="slider" onInput={handleInput} />
+        <p>Value: {price}</p>
+      </div>
+      <style jsx>
+        {`
+          slider::-ms-track {
+            width: 300px;
+            height: 5px;
+
+            /*remove bg colour from the track, we'll use ms-fill-lower and ms-fill-upper instead */
+            background: transparent;
+
+            /*leave room for the larger thumb to overflow with a transparent border */
+            border-color: transparent;
+            border-width: 6px 0;
+
+            /*remove default tick marks*/
+            color: transparent;
+          }
+          slider::-ms-fill-lower {
+            background: #777;
+            border-radius: 10px;
+          }
+          slider::-ms-fill-upper {
+            background: #ddd;
+            border-radius: 10px;
+          }
+          slider::-ms-thumb {
+            border: none;
+            height: 16px;
+            width: 16px;
+            border-radius: 50%;
+            background: goldenrod;
+          }
+          slider:focus::-ms-fill-lower {
+            background: #888;
+          }
+          slider:focus::-ms-fill-upper {
+            background: #ccc;
+          }
+        `}
+      </style>
+    </>
   );
 };
 

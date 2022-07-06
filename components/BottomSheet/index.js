580872f767229bfe37bf4f4feef05dd72f69739b
Yiğitcan <yigitarc45@gmail.com>
2022-07-06 13:41:06 +0300

    Update index.js

--- components/BottomSheet/index.js
+++ components/BottomSheet/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { CupertinoPane } from 'cupertino-pane';
 import Close from '../icons/Close';
 import HeaderTitle from '../HeaderTitle';
@@ -7,26 +7,28 @@ const BottomSheet = ({ onDismiss, isOpen, children, title, leftComponent, classN
   let myPane = useRef();
 
   useEffect(() => {
-    myPane.current = new CupertinoPane(
-      '.' + className,
-      // Pane container selector
-      {
-        buttonDestroy: false,
-        showDraggable: false,
-        backdrop: true,
-        fitHeight: true,
-        parentElement: 'body', // Parent container
-        breaks: {
-          middle: { enabled: true, bounce: true },
-        },
-        events: {
-          onDidDismiss: onDismiss,
-        },
-        fastSwipeClose: true,
-        bottomClose: true,
-        lowerThanBottom: false,
-      }
-    );
+    if (!myPane.current) {
+      myPane.current = new CupertinoPane(
+        '.' + className,
+        // Pane container selector
+        {
+          buttonDestroy: false,
+          showDraggable: false,
+          backdrop: true,
+          fitHeight: true,
+          parentElement: 'body', // Parent container
+          breaks: {
+            middle: { enabled: true, bounce: true },
+          },
+          events: {
+            onDidDismiss: onDismiss,
+          },
+          fastSwipeClose: true,
+          bottomClose: true,
+          lowerThanBottom: false,
+        }
+      );
+    }
 
     if (isOpen) {
       myPane.current.present({ animate: true });
@@ -34,24 +36,22 @@ const BottomSheet = ({ onDismiss, isOpen, children, title, leftComponent, classN
   }, [isOpen]);
 
   return (
-    isOpen && (
-      <div className={className}>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: '24px',
-          }}
-        >
-          {leftComponent || <div />}
-          <HeaderTitle>{title}</HeaderTitle>
-          <Close onClick={() => myPane.current.destroy({ animate: true }).then(onDismiss)} />
-        </div>
-
-        {children}
+    <div className={className} style={{ visibility: 'hidden' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '24px',
+        }}
+      >
+        {leftComponent || <div />}
+        <HeaderTitle>{title}</HeaderTitle>
+        <Close onClick={() => myPane.current.destroy({ animate: true }).then(onDismiss)} />
       </div>
-    )
+
+      {children}
+    </div>
   );
 };
 

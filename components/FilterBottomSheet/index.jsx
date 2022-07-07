110a8a18c4b0684de50e051096bbaa6c49350715
Yiğitcan <yigitarc45@gmail.com>
2022-07-07 08:55:46 +0300

    filter bottom sheet fixes

--- components/FilterBottomSheet/index.jsx
+++ components/FilterBottomSheet/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import FloatingBottomButton from '../FloatingBottomButton';
 import MultipleSlider from './MultipleSlider';
 import SelectBox from './SelectBox';
 import Slider from './Slider';
 
-function FilterBottomSheet() {
+function FilterBottomSheet({ onClose }) {
   const [distance, setDistance] = useState(100);
   const [priceRange, setPriceRange] = useState([100, 2000]);
   const [sortBy, setSortBy] = useState('Distance');
@@ -13,6 +14,8 @@ function FilterBottomSheet() {
       <SelectBox title="Sort By" data={['Distance', 'Price Range', 'Rating']} selected={sortBy} setSelected={setSortBy} />
       <Slider title="Distance" value={distance} setValue={setDistance} minValue={1} maxValue={150} leftText={'1 mi'} rightText={distance + ' mi'} />
       <MultipleSlider title="Price Range" value={priceRange} setValue={setPriceRange} minValue={100} maxValue={2000} leftText={'$' + priceRange[0]} rightText={'$' + priceRange[1]} />
+
+      {(distance !== 100 || priceRange[0] !== 100 || priceRange[1] !== 2000 || sortBy !== 'Distance') && <FloatingBottomButton onClick={onClose}>SEE RESULTS</FloatingBottomButton>}
     </div>
   );
 }

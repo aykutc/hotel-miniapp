4d57ed895c07eba2399a8fac28f38f984e920d17
ferruh <ferruhcihan20@gmail.com>
2022-07-07 11:54:59 +0300

    update rooms

--- components/RecommendedCard/index.js
+++ components/RecommendedCard/index.js
@@ -1,6 +1,5 @@
 import Router from "next/router";
-import React, { useEffect, useState } from "react";
-import Arrow from "../icons/Arrow";
+import React, { useState } from "react";
 import Like from "../icons/Like";
 import OptimizedImage from "../OptimizedImage";
 import styles from "./Recommended.module.css";
@@ -18,42 +17,43 @@ function RecommendedCard({
   favorite,
   info,
   roomSelect,
-  setSelectedRoom,
-  selectedRoom,
+  updateSelectedRooms,
   item,
   ...props
 }) {
   const [isLike, setIsLike] = useState(false);
-  const [selected, setSelected] = useState(false);
-  // console.log(item.id)
 
   return (
     <div
       className={roomSelect ? styles.roomsWrapper : styles.recommendCardWrapper}
       {...props}
-      onClick={!roomSelect ? () => {
-        Router.push({
-          pathname: "/hotel-detail",
-          query: {
-            subTitle,
-            title,
-            block,
-            discountPrice,
-            price,
-            img,
-            imgWebp,
-            imageStyles,
-            showFavorite,
-            favorite,
-          },
-        });
-      } : undefined}
+      onClick={
+        !roomSelect
+          ? () => {
+              Router.push({
+                pathname: "/hotel-detail",
+                query: {
+                  subTitle,
+                  title,
+                  block,
+                  discountPrice,
+                  price,
+                  img,
+                  imgWebp,
+                  imageStyles,
+                  showFavorite,
+                  favorite,
+                },
+              });
+            }
+          : undefined
+      }
     >
       <OptimizedImage
         src={img}
         style={{ ...imageStyles, height: 118 }}
         type={"jpg"}
-      ></OptimizedImage>
+      />
       {showFavorite && (
         <Like
           onClick={() => setIsLike(!isLike)}
@@ -63,17 +63,22 @@ function RecommendedCard({
       )}
 
       <div className={styles.recommendBottomArea}>
-        {subTitle && <div className={styles.tag}>
-          <p className={styles.hotelName}>{subTitle}</p>
-        </div>}
+        {subTitle && (
+          <div className={styles.tag}>
+            <p className={styles.hotelName}>{subTitle}</p>
+          </div>
+        )}
         <div className={styles.recommendTextArea}>
           <p className={styles.recommendTitle}>{title}</p>
-          <p className={styles.recommendDescription}
+          <p
+            className={styles.recommendDescription}
             style={{
               fontSize: roomSelect && "12px",
-              lineHeight: roomSelect && "15px"
-            }}>
-            {info || block}</p>
+              lineHeight: roomSelect && "15px",
+            }}
+          >
+            {info || block}
+          </p>
         </div>
 
         <div className={styles.priceArea}>
@@ -83,15 +88,20 @@ function RecommendedCard({
           </p>
           {roomSelect || <p className={styles.price}>{price}</p>}
         </div>
-        {roomSelect && <button className={styles.roomsButton}
-          onClick={() => {
-            setSelectedRoom(item.id);
-            setSelected(!selected);
-          }}
-          style={{ backgroundColor: selected ? "#1D1F22" : "#e8e9e9", color: selected ? "#fff" : "#565759" }}
-        >
-          {selected ? "SELECTED" : "SELECT"}
-        </button>}
+        {roomSelect && (
+          <button
+            className={styles.roomsButton}
+            onClick={() => {
+              updateSelectedRooms(item.id);
+            }}
+            style={{
+              backgroundColor: item.selected ? "#1D1F22" : "#e8e9e9",
+              color: item.selected ? "#fff" : "#565759",
+            }}
+          >
+            {item.selected ? "SELECTED" : "SELECT"}
+          </button>
+        )}
       </div>
     </div>
   );
--- pages/roomselect/index.js
+++ pages/roomselect/index.js
@@ -1,109 +1,122 @@
+import { useState } from "react";
 import HeaderTitle from "@/components/HeaderTitle";
+import RecommendedCard from "@/components/RecommendedCard";
+import Back from "@/components/icons/Back";
 import styles from "@/styles/Home.module.css";
 import newStyles from "../../components/RecommendedCard/Recommended.module.css";
-import RecommendedCard from "@/components/RecommendedCard";
 import { RoomSelectionArray } from "data/data";
-import Back from "@/components/icons/Back";
-import { useEffect, useState } from "react";
 
-const RoomSelect = ({ accomodationInfo = "Jun24 - Jun 27 (3 Nights)", roomAmount = 2 }) => {
-    const [selectedRooms, setSelectedRooms] = useState([]);
-    const [myRooms, setmyRooms] = useState([]);
-    console.log(myRooms);
+const RoomSelect = ({
+  accomodationInfo = "Jun24 - Jun 27 (3 Nights)",
+  roomAmount = 2,
+}) => {
+  const [selectedRooms, setSelectedRooms] = useState([]);
 
-    // useEffect(() => {
-    //     setSelectedRooms(RoomSelectionArray)
-    // }, [])
+  const updateSelectedRooms = (id) => {
+    const isInclude = selectedRooms.find((item) => item.id === id);
+    if (isInclude || selectedRooms.length < roomAmount) {
+      const index = RoomSelectionArray.findIndex((item) => item.id === id);
+      const selectedRoom = RoomSelectionArray[index];
+      RoomSelectionArray.splice(index, 1, {
+        ...selectedRoom,
+        selected: !selectedRoom.selected,
+      });
+      setSelectedRooms(RoomSelectionArray.filter((e) => e.selected === true));
+    }
+  };
 
-    const updateSelectedRooms = (id) => {
-        const index = RoomSelectionArray.findIndex(item => item.id === id);
-        const selectedRoom = RoomSelectionArray[index];
-        RoomSelectionArray.splice(index, 1, { ...selectedRoom, selected: !selectedRoom.selected });
-        setSelectedRooms(RoomSelectionArray);
-        setmyRooms(RoomSelectionArray.filter(e => e.selected === true));
-    };
-
-    return (
-        <div style={{ marginLeft: 24 }}>
-            <div className={styles.header}>
-                <Back style={{ marginRight: 32 }}></Back>
-                <HeaderTitle>Rooms</HeaderTitle>
+  return (
+    <div style={{ marginLeft: 24 }}>
+      <div className={styles.header}>
+        <Back style={{ marginRight: 32 }}></Back>
+        <HeaderTitle>Rooms</HeaderTitle>
+      </div>
+      <div style={{ height: 16 }}></div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {RoomSelectionArray.map((item) => {
+          return (
+            <div
+              style={{ paddingRight: 24, marginBottom: 16 }}
+              key={item.title}
+            >
+              <RecommendedCard
+                roomSelect
+                updateSelectedRooms={updateSelectedRooms}
+                item={item}
+                title={item.title}
+                info={item.info}
+                img={item.img}
+                price={item.price}
+                imageStyles={{ height: 120, width: "100%", display: "block" }}
+                style={{
+                  width: "100%",
+                  minWidth: "100%",
+                }}
+              />
             </div>
-            <div style={{ height: 16 }}></div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                {RoomSelectionArray.map((item) => {
-                    return (
-                        <div
-                            style={{ paddingRight: 24, marginBottom: 16 }}
-                            key={item.title}
-                        >
-                            <RecommendedCard
-                                setSelectedRoom={updateSelectedRooms}
-                                selectedRoom={item.selected}
-                                item={item}
-                                roomSelect
-                                title={item.title}
-                                info={item.info}
-                                img={item.img}
-                                price={item.price}
-                                imageStyles={{ height: 120, width: "100%", display: "block" }}
-                                style={{
-                                    width: "100%",
-                                    minWidth: "100%",
-                                }}
-                            />
-                        </div>
-                    );
-                })}
-            </div >
-            {myRooms.length > 0 && myRooms.length <= roomAmount &&
-                <div style={{
-                    position: "absolute", bottom: 0, left: 0,
-                    width: "100%", height: "108px",
-                    zIndex: "999",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    padding: "16px 24px 40px",
-                    boxShadow: "inset 0px 1px 0px #E8E9E9"
-                }}>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center"
-                        }}>
-
-                        <div className={newStyles.priceArea}>
-                            <p className={newStyles.discountedPrice}>
-                                ${myRooms.reduce((total, e) => {
-                                    return total + +e.price.slice(1);
-                                }, 0)}
-                                <span>/night</span>
-                            </p>
-
-                        </div>
-                        <p className={newStyles.recommendDescription}
-                            style={{
-                                marginTop: "4px",
-                                marginBottom: 0,
-                                fontWeight: 400
-                            }} >
-                            {accomodationInfo}</p>
-                    </div>
-                    <button style={{
-                        backgroundColor: "#1D1F22",
-                        borderRadius: "104px",
-                        padding: "16px 56px",
-                        color: "#fff",
-                        fontWeight: 400, fontSize: "16px",
-                        lineHeight: "20px"
-                    }}
-                        onClick={() => console.log(myRooms)}
-                    >BOOK</button>
-                </div>
-            }
-        </div >
-    );
+          );
+        })}
+      </div>
+      {selectedRooms.length > 0 && selectedRooms.length <= roomAmount && (
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            height: "108px",
+            zIndex: "999",
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "16px 24px 40px",
+            boxShadow: "inset 0px 1px 0px #E8E9E9",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <div className={newStyles.priceArea}>
+              <p className={newStyles.discountedPrice}>
+                $
+                {selectedRooms.reduce((total, e) => {
+                  return total + +e.price.slice(1);
+                }, 0)}
+                <span>/night</span>
+              </p>
+            </div>
+            <p
+              className={newStyles.recommendDescription}
+              style={{
+                marginTop: "4px",
+                marginBottom: 0,
+                fontWeight: 400,
+              }}
+            >
+              {accomodationInfo}
+            </p>
+          </div>
+          <button
+            style={{
+              backgroundColor: "#1D1F22",
+              borderRadius: "104px",
+              padding: "16px 56px",
+              color: "#fff",
+              fontWeight: 400,
+              fontSize: "16px",
+              lineHeight: "20px",
+            }}
+            onClick={() => console.log(selectedRooms)}
+          >
+            BOOK
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RoomSelect;
